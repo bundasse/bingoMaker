54488b2e86c40041dfd6bb5a5f5f11ff1b72a04e
bundasse <ymtotolove@gmail.com>
2025-10-05 21:11:18 +0900

    016 빙고 컴포넌트 작성

--- src/components/BingoComponent.vue
+++ src/components/BingoComponent.vue
@@ -1,39 +1,19 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+// const props = defineProps(() => {
+
+// })
   const bingoSize =ref(3)
-  const bingoInputBox =ref()
-  const bingoWidth = computed(() => {
-    return (600 - 4*(bingoSize.value-1))/bingoSize.value
-  })
-
-  function changeTextHeight(idx) {
-    let sh = bingoInputBox.value[idx].scrollHeight;
-    if(bingoInputBox.value[idx].value == ''|| bingoInputBox.value[idx].value.length < 6){
-      sh = 26;
-    }
-    bingoInputBox.value[idx].style.height = 'auto';
-    bingoInputBox.value[idx].style.height = sh+'px';
-  }
-
-</script>
+  </script>
 <template>
-  <div>
     <div>
-      빙고 크기
-      <select name="size" id="size" v-model="bingoSize">
-        <option :value="3">3×3</option>
-        <option :value="4">4×4</option>
-        <option :value="5">5×5</option>
-      </select>
-    </div>
-    <div class="bingoArea" :style="`grid: repeat(${bingoSize}, 1fr) / repeat(${bingoSize}, 1fr);`">
+        <div class="bingoArea" :style="`grid: repeat(${bingoSize}, 1fr) / repeat(${bingoSize}, 1fr);`">
       <div class="bingoCell" :style="`width: ${bingoWidth}px; height: ${bingoWidth}px;`" v-for="row in Math.pow(bingoSize,2)" :key="row">
         <textarea :style="`width: ${bingoWidth-3}px;`" ref="bingoInputBox" rows="1" class="bingoInput" @keyup="changeTextHeight(row-1)"></textarea>
       </div>
     </div>
-  </div>
+    </div>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
